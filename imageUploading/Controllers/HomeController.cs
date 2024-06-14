03d89707e433c9f757107a150d86caeffae03a72
Bilal Abdullah <bilal.abdullah@macrosoftinc.com>
2024-06-14 19:09:23 +0500

    06-14-24

---
+++ imageUploading/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using imageUploading.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace imageUploading.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        static Gallery g = new Gallery();
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new Gallery();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Gallery gal, IFormFile img)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(gal);
+            //}
+
+            if (img != null && img.Length > 0)
+            {
+                var fileName = Path.GetFileName(img.FileName);
+                string imgFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/myfiles");
+
+                if (!Directory.Exists(imgFolder))
+                {
+                    Directory.CreateDirectory(imgFolder);
+                }
+
+                string filePath = Path.Combine(imgFolder, fileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    img.CopyTo(stream);
+                }
+
+                string uploadPath = Path.Combine("myfiles", fileName);
+                gal.image = uploadPath;
+            }
+
+            List<string> selectedSkills = new List<string>();
+            //SKILLS
+
+            if (gal.HTML) selectedSkills.Add("HTML");
+            if (gal.CSS) selectedSkills.Add("CSS");
+            if (gal.JavaScript) selectedSkills.Add("JavaScript");
+            if (gal.Python) selectedSkills.Add("Python");
+            if (gal.React) selectedSkills.Add("React");
+            if (gal.Nodejs) selectedSkills.Add("Node.js");
+            if (gal.Angular) selectedSkills.Add("Angular");
+            if (gal.Java) selectedSkills.Add("Java");
+            if (gal.Csharp) selectedSkills.Add("C#");
+
+            List<string> selectedHobbies = new List<string>();
+            //HOBIES
+            if (gal.Reading) selectedHobbies.Add("Reading");
+            if (gal.Sports) selectedHobbies.Add("Sports");
+            if (gal.Music) selectedHobbies.Add("Music");
+            if (gal.Travel) selectedHobbies.Add("Travel");
+            if (gal.Cooking) selectedHobbies.Add("Cooking");
+            if (gal.Gaming) selectedHobbies.Add("Gaming");
+            if (gal.Photography) selectedHobbies.Add("Photography");
+            if (gal.Painting) selectedHobbies.Add("Painting");
+
+            ViewBag.SelectedSkills = selectedSkills;
+            ViewBag.SelectedHobbies = selectedHobbies;
+
+            return View("Gallery", gal); 
+        }
+
+        public IActionResult Gallery()
+        {
+            return View(g);
+        }
+
+    }
+}
---
+++ imageUploading/Models/Gallery.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imageUploading.Models
+{
+    public class Gallery
+    {
+        [Required(ErrorMessage = "Given Name is required.")]
+        [Display(Name = "Given Name")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Image File is required.")]
+        [Display(Name = "Image File")]
+        public string image { get; set; }
+
+        [Required(ErrorMessage = "Family Name is required.")]
+        [Display(Name = "Family Name")]
+        public string FamilyName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Home Address is required.")]
+        [Display(Name = "Home Address")]
+        public string HomeAddress { get; set; }
+
+        [Required(ErrorMessage = "Summary is required.")]
+        [Display(Name = "Summary")]
+        public string Summary { get; set; }
+
+        [Required(ErrorMessage = "Experience is required.")]
+        [Display(Name = "Experience")]
+        public string Experience { get; set; }
+
+        [Required(ErrorMessage = "Education is required.")]
+        [Display(Name = "Education")]
+        public string Education { get; set; }
+
+        [Required(ErrorMessage = "Certificate is required.")]
+        [Display(Name = "Certificate")]
+        public string Certificate { get; set; }
+
+        //SKILLS
+        public bool HTML { get; set; }
+        public bool CSS { get; set; }
+        public bool JavaScript { get; set; }
+        public bool Python { get; set; }
+        public bool React { get; set; }
+        public bool Nodejs { get; set; }
+        public bool Angular { get; set; }
+        public bool Java { get; set; }
+        public bool Csharp { get; set; }
+        public bool PHP { get; set; }
+
+        //HOBIES
+        public bool Reading { get; set; }
+        public bool Sports { get; set; }
+        public bool Music { get; set; }
+        public bool Travel { get; set; }
+        public bool Cooking { get; set; }
+        public bool Gaming { get; set; }
+        public bool Photography { get; set; }
+        public bool Painting { get; set; }
+     
+
+    }
+}
